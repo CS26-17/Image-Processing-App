8e89a724d0f79a04f7c96bfda7bf46d8331245b7
leojia22 <ljia2@zagmail.gonzaga.edu>
2025-10-29 18:31:07 -0700

    hello_world_app.py

--- hello_world_app.py
+++ hello_world_app.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget, QLabel
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget, QLabel, QTabWidget
 from PyQt5.QtCore import Qt
 
 class HelloWorldApp(QMainWindow):
@@ -8,9 +8,11 @@ class HelloWorldApp(QMainWindow):
         self.setWindowTitle("Hello World App")
         self.setGeometry(100, 100, 400, 300)
         
-        central_widget = QWidget()
-        self.setCentralWidget(central_widget)
-        layout = QVBoxLayout(central_widget)
+        self.tab_widget = QTabWidget()
+        self.setCentralWidget(self.tab_widget)
+        
+        home_widget = QWidget()
+        layout = QVBoxLayout(home_widget)
         
         self.label = QLabel("Click the button to see a greeting!")
         self.label.setStyleSheet("font-size: 16px; margin: 20px;")
@@ -32,6 +34,17 @@ class HelloWorldApp(QMainWindow):
         layout.addWidget(self.button, alignment=Qt.AlignmentFlag.AlignCenter)
         
         layout.addStretch()
+        
+        results_widget = QWidget()
+        documentation_widget = QWidget()
+        modification_widget = QWidget()
+        analysis_setup_widget = QWidget()
+        
+        self.tab_widget.addTab(home_widget, "Home")
+        self.tab_widget.addTab(results_widget, "Results")
+        self.tab_widget.addTab(documentation_widget, "Documentation")
+        self.tab_widget.addTab(modification_widget, "Modification")
+        self.tab_widget.addTab(analysis_setup_widget, "Analysis Set up")
     
     def show_greeting(self):
         self.label.setText("Hello, World!")
